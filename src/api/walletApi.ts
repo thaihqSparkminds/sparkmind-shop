9b57a9c01af3d73d9cd0efe2f9bc4bf1b5fe73a6
datnt <datnt@sparkminds.net>
2022-12-15 16:45:54 +0700

    15/12

--- src/api/walletApi.ts
+++ src/api/walletApi.ts
@@ -6,14 +6,7 @@ const walletApi = {
     const url = `wallets`;
     return axiosClient.get(url, config(token));
   },
-  createWallet(token: string, data: WalletInformation): Promise<WalletInformation> {
-    const url = `wallets`;
-    return axiosClient.post(url, data, config(token));
-  },
-  updateWalletBalance(token: string, data: WalletInformation): Promise<void> {
-    const url = `wallets`;
-    return axiosClient.put(url, data, config(token));
-  },
+
   checkWalletExisted(token: string): Promise<boolean> {
     const url = `wallets/check-exist`;
     return axiosClient.get(url, config(token));
